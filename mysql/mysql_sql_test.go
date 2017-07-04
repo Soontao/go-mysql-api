e0658084aa9815130785126a7b6e80a3e531410e
Theo Sun <theo.sun@sap.com>
2017-07-04 09:49:22 +0800

    fix test

--- mysql/mysql_sql_test.go
+++ mysql/mysql_sql_test.go
@@ -1,11 +1,12 @@
 package mysql
 
 import "testing"
+import "net/url"
 
 func TestSQL_GetByTableAndID(t *testing.T) {
 	api := NewMysqlAPI(connectionStr)
 	defer api.Stop()
-	if sql, err := api.sql.GetByTableAndID("monitor", 1); err != nil {
+	if sql, err := api.sql.GetByTableAndID("monitor", 1, url.Values{}); err != nil {
 		t.Error(err)
 	} else {
 		println(sql)
